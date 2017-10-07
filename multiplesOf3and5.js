d6a3bce42b4f8e76673acb3b9101141ea9f36d40
amatranga <amatranga892@gmail.com>
2017-10-06 20:27:40 -0700

    Implements multiples of 3 and 5
    If we list all the natural numbers below 10 that are multiples of 3
    or 5, we get 3, 5, 6 and 9.
    The sum of these multiples is 23.
    
    Find the sum of all the multiples of 3 or 5 below 1000.

---
+++ multiplesOf3and5.js
@@ -0,0 +1,28 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+// The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+const fizBuzz = (max, _arr) => {
+  _arr = [];
+  for (let i = 1; i <= max; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      _arr.push(i);
+    }
+  }
+  return arr;
+};
+
+const reduce = (arr) => {
+  return arr.reduce((a,b) => a + b);
+};
+
+// reduce(fizBuzz(1000));
+
+
+// Because it has to be golfed...
+const golf = (N) => {
+  return Array(N).fill().map((e,i) => (((i + 1) % 3 === 0 || (i + 1) % 5 === 0) && (i + 1 !== N)) ? i + 1 : 0).reduce((a,b) => a + b);
+};
+
+// golf(1000);
