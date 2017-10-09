1a0c5efc27cf4ef62941c21a8895ae92d315c5f6
amatranga <amatranga892@gmail.com>
2017-10-09 13:14:14 -0700

    Implements findNthPrime
    Memoizer can be better

---
+++ nthPrime.ts
@@ -0,0 +1,33 @@
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
+// we can see that the 6th prime is 13.
+
+// What is the 10 001st prime number?
+
+const primes: object = {1:2, 2:3, 3:5, 4:7, 5:11, 6:13};
+const primesArr: Array<number> = [2, 3, 5, 7, 11, 13];
+
+const findNthPrime = (n: number) => {
+
+	if (n <= 0){
+    throw new Error("Not possible");
+  }
+
+  if (primes[n]){ return primes[n]; }
+  for (let num = 15; Object.keys(primes).length < n; num += 2){
+    let isPrime: boolean = true;
+    
+    for (let i = 1; isPrime && i * i <= num; i++){
+      if (num % (primes[i]) === 0){
+        isPrime = false;
+      }
+    }
+
+    if (isPrime && primesArr.indexOf(num) < 0){
+      primesArr.push(num);
+      let index = Object.keys(primes).length + 1;
+      primes[index] = num;
+    }
+  }
+
+  return primes[n];
+};
