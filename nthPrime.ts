a558ae80515c5d756e2952cc52815912ed19bb0c
amatranga <amatranga892@gmail.com>
2017-10-10 19:37:11 -0700

    Update nthPrime

--- nthPrime.ts
+++ nthPrime.ts
@@ -4,30 +4,27 @@
 // What is the 10 001st prime number?
 
 const primes: object = {1:2, 2:3, 3:5, 4:7, 5:11, 6:13};
-const primesArr: Array<number> = [2, 3, 5, 7, 11, 13];
 
 const findNthPrime = (n: number) => {
-
-	if (n <= 0){
-    throw new Error("Not possible");
+  if (n <= 0) {
+    throw new Error('Not possible');
   }
 
-  if (primes[n]){ return primes[n]; }
-  for (let num = 15; Object.keys(primes).length < n; num += 2){
-    let isPrime: boolean = true;
-    
-    for (let i = 1; isPrime && i * i <= num; i++){
-      if (num % (primes[i]) === 0){
+  if (primes[n]) { return primes[n]; }
+
+  for (let num = 15; Object.keys(primes).length < n; num += 2) {
+    let isPrime:boolean = true;
+    for (let i = 1; isPrime && i * i <= num; i++) {
+      if (num % (primes[i]) === 0) {
         isPrime = false;
       }
     }
-
-    if (isPrime && primesArr.indexOf(num) < 0){
-      primesArr.push(num);
-      let index = Object.keys(primes).length + 1;
-      primes[index] = num;
+    if (isPrime && !primes.hasOwnProperty(num)) {
+      const idx = Object.keys(primes).length + 1;
+      primes[idx] = num;
     }
   }
-
   return primes[n];
-};
+}
+
+findNthPrime(10001);
