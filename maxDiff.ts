f894ffe7f1445d481c4e876c4f34d8da2fddc6d5
amatranga <amatranga892@gmail.com>
2017-10-16 19:03:51 -0700

    Slight optimization

--- maxDiff.ts
+++ maxDiff.ts
@@ -2,24 +2,8 @@
 // in an array where the index of a is less than 
 // the index of b.
 
-// const maxDifference = (ints: Array<number>) => {
-//   let maxDiff: number = ints[1] - ints[0];
-//   let min: number = ints[0];
-//   ints.forEach(int => {
-//     if (int > min) {
-//       let diff = Math.abs(int - min);
-//       if (diff > maxDiff) {
-//         maxDiff = diff;
-//       }
-//     } else {
-//       min = int;
-//     }
-//   });
-//   return maxDiff;
-// };
 
-
-// Now with 50% more Beef!
+// Not optimized solution
 const maxDiffer = (nums: Array<number>) => {
   let maxDifference: number = 0;
   nums.forEach((num:number) => {
@@ -36,7 +20,24 @@ const maxDiffer = (nums: Array<number>) => {
   return maxDifference;
 }
 
-const prices = [10, 7, 5, 8, 11, 9, 1];
+// Slight optimization
+const maxDifference = (nums: Array<number>) => {
+  let maxDiff: number = 0;
+  nums.forEach((num: number, i: number) => {
+    for (let j = i; j < nums.length; j++) {
+      let diff: number = 0;
+      if (Math.abs(nums[j] - num) > Math.abs(diff)) {
+        diff = nums[j] - num;
+        if (Math.abs(diff) > Math.abs(maxDiff)) {
+          maxDiff = diff;
+        }
+      }
+    }
+  });
+  return maxDiff;
+}
+
+const prices: Array<number> = [10, 7, 5, 8, 11, 9, 1];
 maxDiffer(prices);
 // => -10
 // The greatest change in prices is between 11 and 1, which is a 
